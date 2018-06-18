958a384ec3f70bbe0360a24d1195701ffad0e6d0
Pratishtha <ppp.pratishtha@gmail.com>
2018-06-18 15:22:09 +0530

    tags added

---
+++ people/methods/tagwords.js
@@ -0,0 +1,19 @@
+module.exports = [ "Access" ,"Account", "Android" ,"Archive" , "Artifical" ,"Application" ,"blog" ,
+    "Bit" ,"Bandwidth" ,"Bluetooth" , "Bios" ,"Broadband" ,"CAD" ,"cache" ,"CD" ,"Code" ,
+    "Cloud" ,"Compilation" ,"Connection" ,"Cookies" ,"Copyright" ,"Cyberspace","Cybersecurity",
+    "Daemon" ,"Data" ,"Database" ,"Debug" ,"Decipher" ,"DevOps" ,"digital" ,"Docker" ,
+    "Email" ,"Ethernet" , "Express" ,"encryption" ,"Facebook" ,"Fax" ,"File" ,"Film",
+    "Firewall" ,"Font" ,"Graphics" ,"Git" ,"Grid" ,"Hacker" ,"Host" ,"Hypertext" ,
+    "Internet" ,"Interface" ,"Intranet" ,"ipad" ,"job" ,"javascript" ,"js" ,"Jenkins" ,
+    "laptop" ,"LAN" ,"link" , "mainframe" ,"matrix" ,"media" ,"memory" ,"message" , "method" ,
+    "mongoDB" ,"ML" ,"machine" ,"learning" ,"MYSQL" ,"modem" ,"monitor" ,"network" ,"node" ,
+    "OS" ,"password" , "pixel" ,"php" ,"privacy" ,"project" ,"protocol" ,"quality" , "query" ,
+    "queue" ,"radio" ,"router" , "rate" ,"Technology" ,"Satellite" ,"scan" ,"Selenium" ,"Skype" ,
+    "smartphone" , "script" ,"terminal" ,"tablet" ,"tagging" ,"tracking" ,"tools" ,"Terminal" ,
+    "URL","virus" ,"video" ,"virtual" ,"virus" ,"visual" ,"Web" ,"windows" ,"Wiki" ,"word" ,
+    "Zombie",
+];
+
+
+
+
