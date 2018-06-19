2f59e789276feb09aeeff00ffed92cb53ca26c90
guptasoumya11 <soumyagupta9c@gmail.com>
2018-06-19 12:09:28 +0530

    env file added

--- people/index.js
+++ people/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 const _find = require('./methods/find');
 const _get = require('./methods/get');
-const _tag = require('./methods/tag');
+
 
 router.get('/', _get);
 
 router.get('/search/:queryValue', _find);
 
-router.get('/tag', _tag);
+
 
 module.exports = router;
