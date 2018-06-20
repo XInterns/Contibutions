38b2055d9c0c164f3b24e6b36a7c034f16ef1f2e
guptasoumya11 <soumyagupta9c@gmail.com>
2018-06-20 10:39:17 +0530

    Test Cases for Search Logic

---
+++ test/test-pages.js
@@ -0,0 +1,41 @@
+var expect  = require('chai').expect;
+var request = require('request');
+var assert = require('chai').assert;
+var baseURL = "http://localhost:5000/"
+
+describe("API test", function(){
+
+    it('Check json objects are returned',function(done){
+        request(baseURL , function(error, response, body){
+            assert(JSON.parse(body), "Converted to JSON")
+            done();
+        });
+    });
+
+
+
+    //To check the query returns some elements that are not  null
+    it('GET /', function(done) {        
+        request(baseURL, function(error, response, body) {
+            let result = JSON.parse(body);
+            expect(body).to.have.length.greaterThan(0);
+            done();
+        });
+    });
+
+    //To check the query parameter is contained in the result object
+    it('GET /search',function(done){
+        request({url :baseURL + 'search?queryparam=xebia'}, function(error, response, body){
+            let result = JSON.parse(body);
+            expect(body).to.contain('xebia');
+            done();
+        });
+    });
+
+    // it('GET /tag',function(done){
+    //     request({url: baseURL + 'tag'}, function(err, response, body){            
+    //         expect(body).to.have.length.greaterThan(10);
+    //         done();
+    //     });
+    // })
+});
